ca7b770a97b56d9ab71a718f93292a75e877cd37
Marc Manning <85353987+MarcAJM@users.noreply.github.com>
2023-05-17 20:22:18 +0200

    Update screen.py

--- screen.py
+++ screen.py
@@ -15,27 +15,30 @@ BL = 18
 bus = 0 
 device = 0 
 
-try:
-    display = LCD_1inch3.LCD_1inch3()
-    display.Init()     # Initialize library.
-    display.clear()    # Clear display.
+# This is for when the machine starts:
+display = LCD_1inch3.LCD_1inch3()
+display.Init()     # Initialize library.
+display.clear()    # Clear display.
 
-    image = Image.new("RGB", (display.width, display.height), "WHITE")    # Create blank image for drawing.
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("../fonts/font1.ttf", 50)
+# Everytime the screen needs to get an update call this method:
+def updateDisplay(speed, white_disks, black_disks, error_message):
+    try:
+        image = Image.new("RGB", (display.width, display.height), "WHITE")    # Create blank image for drawing.
+        draw = ImageDraw.Draw(image)
+        font = ImageFont.truetype("../fonts/font1.ttf", 50)
 
-    draw.text((5, 5), 'Speed: High', fill = 'BLUE',font=font) # Line for the speed of the conveyor belt
-    draw.text((5, 65), 'White disks: 5', fill = 'BLUE',font=font) # Line for the amount of white disks in bin
-    draw.text((5, 125), 'Black disks: 10', fill = 'BLUE',font=font) # Line for the amount of black disks in bin
-    draw.text((5, 185), 'Error', fill = 'RED',font=font) # Line for error description
+        draw.text((5, 5), 'Speed:' + speed, fill = 'BLUE',font=font) # Line for the speed of the conveyor belt
+        draw.text((5, 65), 'White disks:' + white_disks, fill = 'BLUE',font=font) # Line for the amount of white disks in bin
+        draw.text((5, 125), 'Black disks:' + black_disks, fill = 'BLUE',font=font) # Line for the amount of black disks in bin
+        draw.text((5, 185), error_message, fill = 'RED',font=font) # Line for error description
 
-    display.ShowImage(image)
-    display.module_exit()
+        display.ShowImage(image)
+        display.module_exit()
 
-except IOError as e:
-    logging.info(e)    
+    except IOError as e:
+        logging.info(e)    
 
-except KeyboardInterrupt:
-    display.module_exit()
-    logging.info("quit:")
-    exit()
+    except KeyboardInterrupt:
+        display.module_exit()
+        logging.info("quit:")
+        exit()
