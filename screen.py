b7b776309ba3ad9cb9b6c6fa23ba5b0297a1618c
Marc <marcajmanning@icloud.com>
2023-05-17 19:37:24 +0200

    Added lib and fonts and changed screen.py

--- screen.py
+++ screen.py
@@ -3,10 +3,10 @@ import sys
 import time
 import logging
 import spidev as SPI
-sys.path.append("..")
-from lib import LCD_1inch54
 from lib import LCD_1inch3
 from PIL import Image,ImageDraw,ImageFont
+
+sys.path.append("..")
 
 # Raspberry Pi pin configuration:
 RST = 27
@@ -22,8 +22,12 @@ try:
 
     image = Image.new("RGB", (display.width, display.height), "WHITE")    # Create blank image for drawing.
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("../Font/Font02.ttf", 32)
-    draw.text((5, 5), 'Speed: Maximum', fill = 'BLUE',font=font)
+    font = ImageFont.truetype("../fonts/font1.ttf", 50)
+
+    draw.text((5, 5), 'Speed: High', fill = 'BLUE',font=font) # Line for the speed of the conveyor belt
+    draw.text((5, 65), 'White disks: 5', fill = 'BLUE',font=font) # Line for the amount of white disks in bin
+    draw.text((5, 125), 'Black disks: 10', fill = 'BLUE',font=font) # Line for the amount of black disks in bin
+    draw.text((5, 185), 'Error', fill = 'RED',font=font) # Line for error description
 
     display.ShowImage(image)
     display.module_exit()
