eaf2e64dc40ab8009a3a3950a5426fd45eac11d5
Connor <88195050+log-arythmic@users.noreply.github.com>
2023-05-17 11:23:29 +0200

    upen up the limit (yeah)

--- detector.py
+++ detector.py
@@ -1,7 +1,31 @@
 
 detector = 9 + 10
  
-pushUpTheLimit = True
+pushItToTheLimit = True
 
-if(pushUpTheLimit) : 
-    print("Open up the limit")
+if(pushItToTheLimit) : 
+    print("""Push it to the limit
+        Walk along the razor's edge
+        But don't look down just keep your head, or you'll be finished
+        Open up the limit
+        Past the point of no return
+        Reached the top, but still you gotta learn how to keep it
+            Hit the wheel and double the stakes
+        Throttle wide open like a bat outta hell
+        You could crash the gates (crash the gates)
+        Going for the back of beyond
+        Nothing gonna stop you, there's nothing that's wrong
+        So close now you're nearly at the brink
+        So push it, ooh yeah
+        Welcome to the limit (the limit)
+        Take it baby, one step more
+        The power game's still playing so you better win it
+        Push it to the limit (the limit)
+        With no one left to stand in your way
+        You might get careless, but you'll never be safe
+        While you're still livin'
+        Welcome to the limit (the limit)
+        Standing on the razor's edge
+        So don't look down just keep your head, or you'll be finished
+        Welcome to the limit
+        Push it to the limit, hey!""")
