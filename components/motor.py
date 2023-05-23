098d2e807eba803c939ba6fb75f8fee266e8b2ca
Marc Manning <85353987+MarcAJM@users.noreply.github.com>
2023-05-23 19:45:06 +0200

    Add files via upload

--- components/motor.py
+++ components/motor.py
@@ -4,14 +4,12 @@ from time import sleep
 class Motor():
     
     # this is the constructor:
-    def __init__(self, in1, in2, en, speed):
+    def __init__(self, in1, en, speed):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(in1, GPIO.OUT)
-        GPIO.setup(in2, GPIO.OUT)
         GPIO.setup(en, GPIO.OUT) 
 
         GPIO.output(in1, GPIO.HIGH)
-        GPIO.output(in2, GPIO.LOW)
         self.p=GPIO.PWM(en, 1000)
         self.p.start(speed) # Start at maximum speed
     
--- components/solenoid.py
+++ components/solenoid.py
@@ -4,16 +4,20 @@ from time import sleep
 class Solenoid:
     
     # Constructor:
-    def __init__(self, pin):
-        self.pin = pin
+    def __init__(self, in1, en):
+        self.in1 = in1
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(en, GPIO.OUT)
+        
+        GPIO.output(self.in1, GPIO.HIGH)
+        GPIO.output(en, GPIO.HIGH)
 
     # TODO: May not be correct.
     # Push solenoid:
     def push(self, delay):
         sleep(delay)
-        GPIO.output(self.pin, GPIO.LOW)
+        GPIO.output(self.in1, GPIO.LOW)
         sleep(0.05)
-        GPIO.output(self.pin, GPIO.HIGH)
+        GPIO.output(self.in1, GPIO.HIGH)
