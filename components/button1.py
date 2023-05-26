16e3b4b59ad3530b62598e639418a3139311ea5f
Connor <88195050+log-arythmic@users.noreply.github.com>
2023-05-26 12:40:29 +0200

    Co-authored-by: alisha <vitolean@users.noreply.github.com> Co-authored-by: Kerem Gokce <kgokc@users.noreply.github.com>

---
+++ components/button1.py
@@ -0,0 +1,9 @@
+import RPI.GPIO as GPIO
+
+class Button1():
+
+    def __init__(self, in1):
+        self.in1 = in1
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.in1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
