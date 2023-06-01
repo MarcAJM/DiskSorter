4534705d79b164d0dac2ea55d1c7e667029d5a0b
Connor <88195050+log-arythmic@users.noreply.github.com>
2023-06-02 00:51:00 +0200

    First emulation of postApocalyptic mode

---
+++ emulations/postApocalyptic.java
@@ -0,0 +1,24 @@
+package emulations;
+
+class postApocalyptic {
+    double battery1Voltage;
+    double motorVoltage;
+
+    double battery2Voltage;
+    double solenoidVoltage;
+
+    double photoResistance;
+    double resistorVoltage;
+
+    boolean TDRisClosed;
+
+    postApocalyptic() {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        
+    }
+}
